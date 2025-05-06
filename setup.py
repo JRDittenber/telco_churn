c5abfa4d42c6088c956e7eb59448231515093d36
J. Dittenber <90592814+JRDittenber@users.noreply.github.com>
2025-05-05 23:11:36 -0500

    app and setup py updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "Telco Churn Data",
+    version = "0.0.0",
+    author = "J. Dittenber", 
+    author_email = "jdittenber@rose.edu",
+    packages=find_packages()
+    )
